c42db51ce8161b58dc3c5064ffd1ac1a23298f18
asranim <asrani.27@gmail.com>
2020-06-01 20:45:02 +0800

    fix

--- resources/views/laporan/index7.blade.php
+++ resources/views/laporan/index7.blade.php
@@ -27,7 +27,7 @@
       <!-- /.box -->
       <div class="box">
         <div class="box-header">
-          <h3 class="box-title">View Pendapatan</h3>
+          <h3 class="box-title">View Tarif</h3>
         </div>
         <!-- /.box-header -->
         <div class="box-body">
