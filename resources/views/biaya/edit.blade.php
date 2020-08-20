701100620bc94f7e8eb1f1c7e53a0f08977b42dd
asranim <asrani.27@gmail.com>
2020-08-20 16:48:42 +0800

    fix

--- resources/views/biaya/edit.blade.php
+++ resources/views/biaya/edit.blade.php
@@ -6,7 +6,7 @@
         <!-- general form elements -->
         <div class="box box-primary">
           <div class="box-header with-border">
-            <h3 class="box-title">Edit Data Jenis Barang</h3>
+            <h3 class="box-title">Edit Data Biaya Kirim</h3>
           </div>
           <!-- /.box-header -->
           <!-- form start -->
